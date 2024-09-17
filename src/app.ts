8e3cb26d04950f8c01a41df1b5950a45382e444e
techysiddhant <sidjain8000@gmail.com>
2024-09-17 16:25:36 +0530

    register coupon endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import { globalErrorHandler } from "./common/middleware/globalErrorHandler";
 import cookieParser from "cookie-parser";
 import customerRouter from "./customer/customerRouter";
+import couponRouter from "./coupon/couponRouter";
+
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
@@ -10,6 +12,7 @@ app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello from order service service!" });
 });
 app.use("/customer", customerRouter);
+app.use("/coupons", couponRouter);
 app.use(globalErrorHandler);
 
 export default app;
